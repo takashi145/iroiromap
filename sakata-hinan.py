81f2b124d7ec60f12b5f4a2dded7164aac5c6fe2
takashi145 <t.shuhei0203@gmail.com>
2024-02-09 01:58:11 +0900

    指定緊急避難場所マップを１つのファイルにまとめた

--- sakata-hinan.py
+++ sakata-hinan.py
@@ -2,7 +2,7 @@ import pandas as pd
 from io import StringIO
 from bs4 import BeautifulSoup
 import folium
-from folium.plugins import MarkerCluster
+from folium.plugins import MarkerCluster, GroupedLayerControl
 
 def replace_nan(value):
   return '-----' if pd.isna(value) else value
@@ -31,28 +31,26 @@ folium.plugins.Fullscreen(
 data = pd.read_csv('data/hinan.csv')
 sakata_data = data[(data['都道府県名及び市町村名'] == '山形県酒田市')]
 
-marker_cluster = MarkerCluster(
-    name='sakata hinan',
+types = ["洪水", "崖崩れ、土石流及び地滑り", "高潮", "地震", "津波", "大規模な火事", "内水氾濫"]
+
+groups = []
+for type in types:
+  filtered_data = sakata_data[sakata_data[type] == 1]
+  group = folium.FeatureGroup(name=type)
+  groups.append(group)
+
+  marker_cluster = MarkerCluster(
+    name=type,
     overlay=True,
     control=False,
     icon_create_function=None
-)
-
-types = [
-  { 'key': '洪水', 'fileName': 'hinan/floodData.html' },
-  { 'key': '崖崩れ、土石流及び地滑り', 'fileName': 'hinan/landslideData.html' },
-  { 'key': '高潮', 'fileName': 'hinan/stormSurgeData.html' },
-  { 'key': '地震', 'fileName': 'hinan/earthquakeData.html' },
-  { 'key': '津波', 'fileName': 'hinan/tsunamiData.html' },
-  { 'key': '大規模な火事', 'fileName': 'hinan/fireData.html' },
-  { 'key': '内水氾濫', 'fileName': 'hinan/inlandFloodingData.html' },
-]
-
-for type in types:
-  filtered_data = sakata_data[sakata_data[type['key']] == 1]
+  )
 
   for _, row in filtered_data.iterrows():
-    marker = folium.Marker([row['緯度'], row['経度']])
+    marker = folium.Marker(
+      [row['緯度'], row['経度']], 
+      tags=[type]
+    )
     popup = f"""
     <div id="popup">
       <h3>{row['施設・場所名']}</h3>
@@ -61,34 +59,40 @@ for type in types:
     </div>
     """
     folium.Popup(popup).add_to(marker)
-    marker_cluster.add_child(marker)
+    marker.add_to(marker_cluster)
+    marker_cluster.add_to(group)
+    
+  map.add_child(group)
 
-  marker_cluster.add_to(map)
+GroupedLayerControl(
+  groups={'災害': groups},
+  collapsed=False,
+).add_to(map)
 
-  map.save(type["fileName"])
+map.save("hinan.html")
 
-  with open(type["fileName"], 'r', encoding='utf-8') as file:
-    soup = BeautifulSoup(file, 'html.parser')
+with open("hinan.html", 'r', encoding='utf-8') as file:
+  soup = BeautifulSoup(file, 'html.parser')
 
-  link = soup.new_tag('link', rel="stylesheet", href="../main.css")
-  soup.head.append(link)
+link = soup.new_tag('link', rel="stylesheet", href="../main.css")
+soup.head.append(link)
 
-  title = soup.new_tag('title')
-  title.string = f"{type['key']} | 山形県酒田市 指定緊急避難場所マップ"
-  soup.head.append(title)
+title = soup.new_tag('title')
+title.string = f"山形県酒田市 指定緊急避難場所マップ"
+soup.head.append(title)
 
-  link = soup.new_tag('link', rel="stylesheet", href="../main.css")
-  soup.head.append(link)
+link = soup.new_tag('link', rel="stylesheet", href="../main.css")
+soup.head.append(link)
 
-  header = soup.new_tag('h1')
-  header.string = f"山形県酒田市 指定緊急避難場所マップ（{type['key']}）"
-  soup.body.insert(0, header)
+header = soup.new_tag('h1')
+header.string = f"山形県酒田市 指定緊急避難場所マップ"
+soup.body.insert(0, header)
 
-  attribution = f"""
-  <p id="attribution">この「山形県酒田市 指定緊急避難場所マップ（{type['key']}）」は、「指定緊急避難場所の全国データ(公開している市区町村)」(国土地理院)(https://www.gsi.go.jp/bousaichiri/hinanbasho.html#info2)を加工して作成しています。</p>
-  """
-  soup.body.append(BeautifulSoup(attribution, 'html.parser'))
+attribution = f"""
+<p id="attribution">この「山形県酒田市 指定緊急避難場所マップ」は、「指定緊急避難場所の全国データ(公開している市区町村)」(国土地理院)(https://www.gsi.go.jp/bousaichiri/hinanbasho.html#info2)を加工して作成しています。</p>
+"""
+soup.body.append(BeautifulSoup(attribution, 'html.parser'))
 
-  with open(type["fileName"], 'w', encoding='utf-8') as file:
-    file.write(str(soup))
+with open("hinan.html", 'w', encoding='utf-8') as file:
+  file.write(str(soup))
 
